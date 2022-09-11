a13d34bc34e422d3fbaa5f499ba0227f00057ede
muhammad-khair <muhammad_khair@outlook.com>
2022-09-12 01:32:03 +0800

    Move repository logic

--- matching-service/index.js
+++ matching-service/index.js
@@ -2,21 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import router from './controller/matching-routes.js';
 import { createServer } from 'http';
-import { matchingRepository } from './model/repository.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-
-matchingRepository.authenticate()
-    .then((result) => {
-        console.log("Connection established.");
-    })
-    .catch((error) => {
-        console.log("Unable to connect to db: ", error);
-    });
 
 app.get('/', (req, res) => {
     res.send('Hello World from matching-service');
--- matching-service/model/repository.js
+++ matching-service/model/repository.js
@@ -1,9 +1,16 @@
-import 'dotenv/config';
-import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 const matchingRepository = new Sequelize({
     dialect: 'sqlite',
     storage: 'database.sqlite3',
 });
+
+matchingRepository.authenticate()
+    .then((result) => {
+        console.log("Connection established.");
+    })
+    .catch((error) => {
+        console.log("Unable to connect to db: ", error);
+    });
 
 export { matchingRepository };
