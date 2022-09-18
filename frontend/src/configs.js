dd75055a3c05290817cfaae3f84aae624f499a77
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2022-09-18 13:27:54 +0800

    move metadata to configs file

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -8,3 +8,7 @@ const PREFIX_ROOM_SVC = '/api/room';
 export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
 export const URL_MATCHING_MATCH_SVC = URI_MATCHING_SVC + PREFIX_MATCH_SVC;
 export const URL_MATCHING_ROOM_SVC = URI_MATCHING_SVC + PREFIX_ROOM_SVC;
+
+export const AUTH0_DOMAIN = "elgoh.us.auth0.com"
+export const AUTH0_CLIENT_ID = "3CVQjahdc8e1z1cUdqsZMw15FfhqylxW"
+export const AUTH0_SCOPE = "read:current_user update:current_user_metadata"
