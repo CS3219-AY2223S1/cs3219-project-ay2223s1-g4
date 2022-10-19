115da753c8a77c1d24c5f1405bde6315643d9de3
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2022-10-19 23:28:01 +0800

    Remove legacy files

--- questionscraper/app/question/create_tables.py
+++
@@ -1,9 +0,0 @@
-from connector import Base, connect_tcp_socket
-from question_model import *
-from sqlalchemy import MetaData
-engine = connect_tcp_socket().connect()
-
-
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
-
