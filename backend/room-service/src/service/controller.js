0b695f6d08e3226cc326a0669cd35c7d04aaa78d
muhammad-khair <muhammad_khair@outlook.com>
2022-11-01 16:25:45 +0800

    Add sorting for user rooms

--- backend/room-service/src/service/controller.js
+++ backend/room-service/src/service/controller.js
@@ -20,12 +20,27 @@ let RoomController = {
             let sessionEndTime = (!session || session.isOpen) ? null : session.updatedAt;
             let peerId = (userId === room.userid1) ? room.userid2 : room.userid1;
             return {
+                roomId: room._id.toHexString(),
                 startDateTime: room._id.getTimestamp(),
                 endDateTime: sessionEndTime,
                 peerNickname: await getUserNickname(peerId, jwtToken),
                 questionTitle: await getQuestionTitle(room.questionid, jwtToken),
             };
         }));
+
+        populatedUserRooms.sort((detailsA, detailsB) => {
+            if (detailsA.startDateTime.getDate() != detailsB.startDateTime.getDate()) {
+                return (detailsA.startDateTime.getDate() < detailsB.startDateTime.getDate())
+                    ? -1
+                    : 1;
+            }
+            if (detailsA.startDateTime.getTime() != detailsB.startDateTime.getTime()) {
+                return (detailsA.startDateTime.getTime() < detailsB.startDateTime.getTime())
+                    ? -1
+                    : 1;
+            }
+            return 0;
+        });
         console.log(`Updated rooms are ${JSON.stringify(populatedUserRooms)}`);
 
         return res.status(HttpStatus.OK).json(populatedUserRooms);
