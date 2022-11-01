e7bb9231a9cf4029ab273cffa98b8dcfebc9969a
Elgoh <elgoh3@gmail.com>
2022-11-01 16:43:03 +0800

    Add user history table with dummy data

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -9,12 +9,14 @@ const PREFIX_USER_SVC = "/api/user";
 const PREFIX_MATCH_SVC = "/api/match";
 const PREFIX_ROOM_SVC = "/api/room";
 const PREFIX_QUESTION_SVC = "/api/question";
+const PREFIX_USER_HISTORY = "/api/room/user/";
 
 export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
 export const URL_MATCHING_MATCH_SVC = URI_MATCHING_SVC + PREFIX_MATCH_SVC;
 export const URL_MATCHING_ROOM_SVC = URI_ROOM_SVC + PREFIX_ROOM_SVC;
 export const URL_QUESTION = URI_QUESTION_SVC + PREFIX_QUESTION_SVC;
 export const URL_COLLAB_SVC = URI_COLLAB_SVC + PREFIX_COLLAB_SVC;
+export const URL_HISTORY_ENDPT = URI_ROOM_SVC + PREFIX_USER_HISTORY;
 
 export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
 export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
--- frontend/src/pages/UserHistoryPage.js
+++ frontend/src/pages/UserHistoryPage.js
@@ -3,16 +3,65 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Link from "@mui/material/Link";
+import { useNavigate } from "react-router-dom";
 import Loading from "../components/loading";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import { URL_HISTORY_ENDPT } from '../configs';
+
 
 function UserHistoryPage() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+
+  const {history, updateHistory} = useState([])  
+  let nevigateTo = useNavigate();
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return <div></div>;
+  const loadHistory = () => {
+    getAccessTokenSilently().then((token) => {
+      axios.post(URL_HISTORY_ENDPT + encodeURI(user.id), {
+        headers: { Authorization: `Bearer ${token}` },
+      }).then((res) => {
+        updateHistory(res)
+      });
+    })
+  }
+
+  // GET 8002/api/room/user/ + {encodeURI(user.id)}
+  return isAuthenticated && (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Peer name</th>
+          <th>Question title</th>
+          <th>Ongoing</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default UserHistoryPage;
