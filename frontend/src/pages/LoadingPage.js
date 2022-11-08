259d5034c68d98755bb91941d63c54cd9c66a87d
Elgoh <elgoh3@gmail.com>
2022-11-08 15:54:34 +0800

    Update loading page and loading component

--- frontend/src/pages/LoadingPage.js
+++ frontend/src/pages/LoadingPage.js
@@ -1,61 +1,71 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Countdown from 'react-countdown';
-import { useParams, useNavigate, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { io } from 'socket.io-client'; 
-import { URI_MATCHING_SVC } from '../configs';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Countdown from "react-countdown";
+import { useParams, useNavigate, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { URI_MATCHING_SVC } from "../configs";
 import { useAuth0 } from "@auth0/auth0-react";
-import Loading from '../components/loading';
+import Loading from "../components/loading";
 
 function LoadingPage() {
-    const { loadingId: matchid } = useParams();
-    const { isAuthenticated, isLoading } = useAuth0();
+  const { loadingId: matchid } = useParams();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-    let navigateTo = useNavigate();
+  let navigateTo = useNavigate();
 
-    useEffect(() => {
-        const socket = io(URI_MATCHING_SVC);
-        socket.on('connect', () => {
-            console.log(`Connected with id ${socket.id}`);
-        });
-        socket.emit('join-room', `match-${matchid}`);
+  useEffect(() => {
+    const socket = io(URI_MATCHING_SVC);
+    socket.on("connect", () => {
+      console.log(`Connected with id ${socket.id}`);
+    });
+    socket.emit("join-room", `match-${matchid}`);
 
-        const timeoutId = setTimeout(() => {
-            console.log('Matching failed');
-            alert('No peer found for matching! Please try again another time');
-            socket.disconnect(true);
-            console.log(socket);
-            navigateTo('../selectdifficulty');
-        }, (30 + 1) * 1000);
-        
-        socket.on('provide-room', (roomid) => {
-            console.log(`server provided room ${roomid}`);
-            socket.disconnect(true);
-            console.log(socket);
-            clearTimeout(timeoutId);
-            navigateTo(`../room/${roomid}`);
-            return;
-        });
-    }, [matchid, navigateTo]);
-    
-    if (isLoading) {
-        return <Loading/>;
-    }
-    
-    if (!isAuthenticated) {
-        return <Navigate to="/" replace />;
-    }
+    const timeoutId = setTimeout(() => {
+      console.log("Matching failed");
+      alert("No peer found for matching! Please try again another time");
+      socket.disconnect(true);
+      console.log(socket);
+      navigateTo("../selectdifficulty");
+    }, (30 + 1) * 1000);
 
-    return (
-        <Box>
-            <Typography>Finding a peer for you. Hold on...</Typography>
-            <Countdown 
-                date={Date.now() + 30 * 1000}
-            >
-            </Countdown>
-        </Box>
-    );
+    socket.on("provide-room", (roomid) => {
+      console.log(`server provided room ${roomid}`);
+      socket.disconnect(true);
+      console.log(socket);
+      clearTimeout(timeoutId);
+      navigateTo(`../room/${roomid}`);
+      return;
+    });
+  }, [matchid, navigateTo]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <Box
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography>Finding a peer for you. Hold on...</Typography>
+      <br />
+      <br />
+      <br />
+      <Loading />
+      <br />
+      <br />
+      <br />
+      <Countdown date={Date.now() + 30 * 1000}></Countdown>
+    </Box>
+  );
 }
 
 export default LoadingPage;
