75a4e522479d3f484eca3023d226fd9105b57cdc
Elgoh <elgoh3@gmail.com>
2022-11-09 13:45:30 +0800

    Update UI

--- frontend/src/pages/LoadingPage.js
+++ frontend/src/pages/LoadingPage.js
@@ -7,10 +7,39 @@ import { io } from "socket.io-client";
 import { URI_MATCHING_SVC } from "../configs";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "../components/loading";
+import "./LoadingPage.css";
 
 function LoadingPage() {
   const { loadingId: matchid } = useParams();
   const { isAuthenticated, isLoading } = useAuth0();
+
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    // Render a countdown
+    return <span>{seconds}</span>;
+  };
+  const quotes = [
+    { quote: "“If you can DREAM it, you can DO it.” – Walt Disney" },
+    {
+      quote:
+        "“Choose a job you love, and you will never have to work a day in your life.” – Confucius",
+    },
+    {
+      quote:
+        "“Close your eyes. Focus on making yourself feel excited, powerful. Imagine yourself destroying goals with ease.” – Andrew Tate",
+    },
+  ];
+
+  function getRandomQuote(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item.quote;
+  }
+
+  const quote = getRandomQuote(quotes);
 
   let navigateTo = useNavigate();
 
@@ -56,11 +85,29 @@ function LoadingPage() {
           justifyContent: "center",
         }}
       >
-        Finding a peer for you. Hold on for...
-        <Countdown date={Date.now() + 30 * 1000}></Countdown>
+        <div className="roboto">
+          Finding a peer for you. Hold on for...
+          <Countdown
+            date={Date.now() + 30 * 1000}
+            renderer={renderer}
+          ></Countdown>
+          ... seconds
+        </div>
       </Typography>
 
       <Loading />
+      <Typography
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontWeight: "bold",
+        }}
+      >
+        <div className="cursive-quote" style={{ fontSize: 40 }}>
+          {quote}
+        </div>
+      </Typography>
     </Box>
   );
 }
